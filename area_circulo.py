bcadce3f0dbf9c0357b5207ffcd8f4f0b98a8404
herwaldo <herwaldo@hotmail.com>
2015-09-13 21:51:42 -0500

    subir area del circulo y inversion discreta
    en este se sube la inversion discreta y el area del circulo que no se
    subio la anterior vez con la correccion que se hizo a la suma del angulo

--- area_circulo.py
+++ area_circulo.py
@@ -24,18 +24,11 @@ limite=0.0
 
 def congruencialMixto(entrada):
 	Xn = ((a*entrada)+b)%m
-	return Xn/m
+	return Xn
 
 def congruencialMixtoXn(entrada):
 	Xn = ((a*entrada)+b)%m
 	return Xn
-
-def dentro(rando_x,rando_y):
-	valor=rando_x**2+rando_y**2
-	if valor<=(radio**2):
-		print "Este valor esta dentro del circulo."
-	else:
-		print "Este valor no esta dentro del circulo."
 
 def convertir(numero):
 	val=((2*k)*numero)-k
@@ -47,31 +40,18 @@ radio = input("Ingrese el radio del circulo: \n")+0.0
 rando_x=congruencialMixto(x0)
 gerX=congruencialMixtoXn(x0)
 while i<limite:
-	rando_y=congruencialMixto(gerX)
+	rando_y=congruencialMixto(gerX)/m
 	gerY=congruencialMixtoXn(gerX)
-	rando_x=congruencialMixto(gerY)
+	rando_x=congruencialMixto(gerY)/m
 	gerX=congruencialMixtoXn(gerY)
 	x=convertir(rando_x)
 	y=convertir(rando_y)
 	datosX.append(x)
 	datosY.append(y)
-	#dentro(x,y)
-	#print "Esta es la cordenada: "+str(x)+", "+str(y)
 	i+=1
 	valor=rando_x**2+rando_y**2
 	if valor<=(radio**2):
-		delta+=(1/(limite*((2*radio)**2)))
-		print delta
-	'''if (control==100):
-		control=0
-		plt.ion()
-		ax=plt.gca()
-		for z in range(int(i)):
-			if valor<=(radio**2):
-				ax.plot(datosX[z],datosY[z],"b*")
-			else:
-				ax.plot(datosX[z],datosY[z],"r*")
-		plt.draw()'''
+		delta+=((1/limite)*((2*radio)**2))
 	plt.ion()
 	ax=plt.gca()
 	if valor<=(radio**2):
@@ -80,4 +60,5 @@ while i<limite:
 		ax.plot(datosX[control],datosY[control],"r*")
 	plt.draw()
 	control+=1
-	#print "Este es el radio generado: "+str(delta)+", este es el radio teorico: "+str(math.pi*(radio**2))
+
+print "Este es el radio generado: "+str(delta)+", este es el radio teorico: "+str(math.pi*(radio**2))
---
+++ inversion.py
@@ -0,0 +1,39 @@
+import math
+a=5.0
+b=3.0
+m=16.0
+suma=0.0
+x=[]
+px=[]
+acum_sum=0.0
+acumulada=[]
+
+def congruencialMixto(entrada):
+	Xn = ((a*entrada)+b)%m
+	return Xn
+
+def comparar_valores(valor):
+	numero=0.0
+	for i in range(len(px)):
+		if (valor<=acumulada[i]):
+			numero=x[i]
+	return numero
+
+valor = input("Ingrese el numero de variables de aleatorias \n")
+for i in range(valor):
+	dato=input("Ingrese el valor de la variable X "+str(i)+": \n")
+	x.append(dato)
+	porba=input("Ingrese el valor de la Provabilidad de X: \n")
+	px.append(porba+0.0)
+	acum_sum+=porba
+	acumulada.append(acum_sum)
+
+dados=input("ingrese el numero de veces a tirar los dados: \n")
+semilla=input("Ingrese la semilla por favor \n")
+envio=congruencialMixto(semilla)
+
+for i in range(dados):
+	envio=congruencialMixto(envio)
+	suma+=comparar_valores(envio/m)
+
+print suma
